cb413f56ad5ccbc26bf7e556ab7116ce27a43c5d
n-iterations <dev@n-iterations.com>
2013-04-25 23:52:29 +0100

    first commit

---
+++ src/com/niterations/utils/WrongFragmentTypeError.java
@@ -0,0 +1,14 @@
+package com.niterations.utils;
+
+public class WrongFragmentTypeError extends Error
+{
+    /**
+     * Default serial UID
+     */
+    private static final long serialVersionUID = -2212044216100250254L;
+
+    public WrongFragmentTypeError(String msg)
+    {
+        super(msg);
+    }
+}
