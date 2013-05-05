f849ca93c0b6d09504ef99c29c9baec58b973af2
n-iterations <dev@n-iterations.com>
2013-05-05 22:07:58 +0100

    Remove unnecessary comments

--- src/com/niterations/utils/gui/FlexibleScrollView.java
+++ src/com/niterations/utils/gui/FlexibleScrollView.java
@@ -13,35 +13,27 @@ public class FlexibleScrollView extends ScrollView
     public FlexibleScrollView(Context context, AttributeSet attrs, int defStyle)
     {
         super(context, attrs, defStyle);
-        // TODO Auto-generated constructor stub
     }
 
     public FlexibleScrollView(Context context, AttributeSet attrs)
     {
         super(context, attrs);
-        // TODO Auto-generated constructor stub
+
     }
 
     public FlexibleScrollView(Context context)
     {
         super(context);
-        // TODO Auto-generated constructor stub
+
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent ev)
     {
-//        if (!mDisableScrolling)
-//            return super.onTouchEvent(ev);
-//        
-//        return false;  // do nothing
-        
-        
         switch (ev.getAction()) {
         case MotionEvent.ACTION_DOWN:
-            // if we can scroll pass the event to the superclass
+
             if (!mDisableScrolling) return super.onTouchEvent(ev);
-            // only continue to handle the touch event if scrolling enabled
             return !mDisableScrolling; // mScrollable is always false at this point
         default:
             return super.onTouchEvent(ev);
